34e3553a730e267360d89a22f8d9b597c20ce401
anselmos <anselmos@users.noreply.github.com>
2017-05-21 13:44:00 +0200

    Refactors - removes notused imports

--- app/src/main/java/com/udacity/stockhawk/data/MockedHistoricalQuote.java
+++ app/src/main/java/com/udacity/stockhawk/data/MockedHistoricalQuote.java
@@ -1,12 +1,10 @@
 package com.udacity.stockhawk.data;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.Locale;
 import java.util.Random;
 
 import yahoofinance.histquotes.HistoricalQuote;
